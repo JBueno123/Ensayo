e237d9cad82e6f83d9dd2952e02c9348482b6653
Estudiante <Estudiante@BE401S103C09ING.unal.loc>
2019-04-03 08:55:48 -0500

    comandos

--- src/pto/Pto.java
+++ src/pto/Pto.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package pto;
-
+import java.util.*;
 /**
  *
  * @author Estudiante
@@ -15,7 +15,11 @@ public class Pto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       System.out.println("Puto xd");    
+       System.out.println("Puto xd");  
+       Scanner lectura = new Scanner(System.in);
+        System.out.println("Digite un numero");
+        int i = lectura.nextInt();
+        System.out.println("ingreso " + i + "\n" +"es usted mk" );
     }
     
 }
